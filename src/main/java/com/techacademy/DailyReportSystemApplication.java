a0e4dc427f39bcf1e128a41c70732ede4291ae5d
TakanoriShima <quark2galaxy@gmail.com>
2023-05-22 12:59:19 +0900

    mod

--- src/main/java/com/techacademy/DailyReportSystemApplication.java
+++ src/main/java/com/techacademy/DailyReportSystemApplication.java
@@ -2,12 +2,19 @@ package com.techacademy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DailyReportSystemApplication {
+public class DailyReportSystemApplication extends SpringBootServletInitializer {
+//public class DailyReportSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DailyReportSystemApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DailyReportSystemApplication.class);
+	}
 }
